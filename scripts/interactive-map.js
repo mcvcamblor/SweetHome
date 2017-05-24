488a4768f9887a464ee2b1decbd7ca833a9c109b
puyhg5 <puyab500@gmail.com>
2017-05-24 10:24:49 +0200

    styles v4

---
+++ scripts/interactive-map.js
@@ -0,0 +1,16 @@
+var inputMoney = document.querySelector(".range-one");
+var colormap = document.querySelector(".st13");
+colormap.setAttribute("fill", "#7A8417");
+
+var changeColor = function(){
+  var controlOne = inputMoney.value;
+  if (controlOne < 1000){
+    colormap.setAttribute("fill", "green");
+  }else if (controlOne >= 1000 && controlOne < 2000){
+    colormap.setAttribute("fill", "yellow");
+  }else{
+    colormap.setAttribute("fill", "red");
+  }
+}
+
+inputMoney.addEventListener("input", changeColor);
