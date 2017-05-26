744b0cbd7b36032dfe19fdcb6f171c025558bd14
puyhg5 <puyab500@gmail.com>
2017-05-26 17:01:46 +0200

    Header fixed and author cite

---
+++ scripts/scroll-header.js
@@ -0,0 +1,16 @@
+var header = document.querySelector(".header");
+
+function headerFixed() {
+  var viewportSize = 85 * window.innerHeight / 100;
+  var currentViewportSize = window.scrollY;
+  console.log(currentViewportSize);
+  if(window.scrollY > viewportSize){
+    header.style.position = "fixed";
+    header.style.top = 0;
+    header.style.zIndex = 999;
+  }else{
+    header.style.position = "static";
+  }
+}
+
+window.addEventListener('scroll', headerFixed);
