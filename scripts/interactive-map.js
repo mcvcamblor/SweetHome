410f605af6f0f4c788ec4475574ba624af967f74
puyhg5 <puyab500@gmail.com>
2017-05-27 21:06:29 +0200

    Upgrade 2

--- scripts/interactive-map.js
+++ scripts/interactive-map.js
@@ -1,3 +1,35 @@
+'use strict';
+
+//Lo que ocurre cuando haces click en el botón de Madrid o Barcelona
+
+var buttonMadrid = document.querySelector(".btn-madrid");
+var buttonBarcelona = document.querySelector(".btn-barcelona");
+var mapMadrid = document.querySelector(".map-madrid");
+var mapBarcelona = document.querySelector(".map-barcelona");
+
+function changeMapMadrid(){
+  buttonMadrid.classList.add("btn-focus");
+  buttonBarcelona.classList.remove("btn-focus");
+  mapMadrid.classList.add("on");
+  mapBarcelona.classList.add("off");
+  mapMadrid.classList.remove("off");
+  mapBarcelona.classList.remove("on");
+}
+
+function changeMapBarcelona(){
+  buttonBarcelona.classList.add("btn-focus");
+  buttonMadrid.classList.remove("btn-focus");
+  mapMadrid.classList.add("off");
+  mapBarcelona.classList.add("on");
+  mapMadrid.classList.remove("on");
+  mapBarcelona.classList.remove("off");
+}
+
+buttonMadrid.addEventListener("click", changeMapMadrid);
+buttonBarcelona.addEventListener("click", changeMapBarcelona);
+
+
+//Lo que ocurre cuando cambias el dinero
 var inputMoney = document.querySelector(".range-one");
 var colormap = document.querySelector(".st13");
 colormap.setAttribute("fill", "#7A8417");
@@ -11,6 +43,6 @@ var changeColor = function(){
   }else{
     colormap.setAttribute("fill", "red");
   }
-}
+};
 
 inputMoney.addEventListener("input", changeColor);
