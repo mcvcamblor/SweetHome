b697a304af63b64a4553a50c1458701d740fc6c0
LRodriAce <LRodriAce@gmail.com>
2017-05-29 23:01:21 +0200

    Dejamos el cálculo ideal para 1200€

--- scripts/expensesCalculation.js
+++ scripts/expensesCalculation.js
@@ -15,49 +15,49 @@ function calculateExpenses(salary, houseRentPercent) {
   //         'otros': [42, 42]
   //       }
 
-    // // EJEMPLO DE CALCULO
-    // // Calculo
-    // // Porcentaje Ideal
-    // // 32%
-    // tipoYPorcentaje =  {'alimentacion': 13, 'ropa': 4, 'transporte': 11, 'salud': 4, 'hogar': 4,'ensenanza': 1, 'telecomunicacion': 3, 'ahorros': 7, 'bares': 9, 'cultura': 4, 'tabaco': 2, 'otros': 6};
-    // var result = {};
-    // var x = 0;
-    // var data = null;
-    // for (var key in tipoYPorcentaje) {
-    //   x = ( salary * tipoYPorcentaje[key] ) / 100;
-    //   // var data = {key: [x, tipoYPorcentaje[key]]};
-    //   // console.log("data:"+data);
-    //   result[key] = [x, tipoYPorcentaje[key]];
-    //   console.log("añadimos key:"+key);
-    //   console.log("añadimos valor:"+result[key]);
-    // }
-    //
-    // // La respuesta
-    // var expectResult = {'alimentacion': [156, 13],
-    //  'ropa': [48, 4],
-    //  'transporte': [132, 11],
-    //  'salud': [48, 4],
-    //  'hogar': [48, 4],
-    //  'ensenanza': [12, 1],
-    //  'telecomunicacion': [36, 3],
-    //  'ahorros': [84, 7],
-    //  'bares': [108, 9],
-    //  'cultura': [48, 4],
-    //  'tabaco': [24, 2],
-    //  'otros': [72, 6]
-    // }
-    //
-    //
-    // console.log("La respuesta esperada");
-    // for (var key in expectResult) {
-    //   console.log("clave:"+key+ " valor:"+expectResult[key]);
-    // }
-    // console.log("----------");
-    // console.log("Lo calculado:");
-    // for (var key in result) {
-    //   console.log("clave:"+key+ " valor:"+result[key]);
-    // }
-    // return result;
+    // EJEMPLO DE CALCULO
+    // Calculo
+    // Porcentaje Ideal
+    // 32%
+    tipoYPorcentaje =  {'alimentacion': 13, 'ropa': 4, 'transporte': 11, 'salud': 4, 'hogar': 4,'ensenanza': 1, 'telecomunicacion': 3, 'ahorros': 7, 'bares': 9, 'cultura': 4, 'tabaco': 2, 'otros': 6};
+    var result = {};
+    var x = 0;
+    var data = null;
+    for (var key in tipoYPorcentaje) {
+      x = ( salary * tipoYPorcentaje[key] ) / 100;
+      // var data = {key: [x, tipoYPorcentaje[key]]};
+      // console.log("data:"+data);
+      result[key] = [x, tipoYPorcentaje[key]];
+      console.log("añadimos key:"+key);
+      console.log("añadimos valor:"+result[key]);
+    }
+
+    // La respuesta
+    var expectResult = {'alimentacion': [156, 13],
+     'ropa': [48, 4],
+     'transporte': [132, 11],
+     'salud': [48, 4],
+     'hogar': [48, 4],
+     'ensenanza': [12, 1],
+     'telecomunicacion': [36, 3],
+     'ahorros': [84, 7],
+     'bares': [108, 9],
+     'cultura': [48, 4],
+     'tabaco': [24, 2],
+     'otros': [72, 6]
+    }
+
+
+    console.log("La respuesta esperada");
+    for (var key in expectResult) {
+      console.log("clave:"+key+ " valor:"+expectResult[key]);
+    }
+    console.log("----------");
+    console.log("Lo calculado:");
+    for (var key in result) {
+      console.log("clave:"+key+ " valor:"+result[key]);
+    }
+    return result;
 
     // EJEMPLO DE CALCULO
     // Calculo
